6e00d38a3d717fe605fd9a70bfa109db66d0b4eb
Julian Pereira <blueshade@gmail.com>
2014-08-11 05:46:47 -0400

    More fixes for accuracy of data

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ module.exports = {
 		// 	console.log( d.moment.toString(), ':', d.data.season );
 		// });
 
-		var res = formatters.getMondays( liturgicalDates );
+		var res = formatters.getTuesdays( liturgicalDates );
 		lodash.map( res, function( d, key ) {
 			console.log( d.moment.toString(), ':', d.name, ':', d.type );
 		});
