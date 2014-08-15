411ddb48f132c9301efa67ba4c2e861514d68823
Julian Pereira <bleushade@gmail.com>
2014-08-16 01:45:06 +0800

    v1.0.4: Fixes for buggy national calendar

--- test/index.js
+++ test/index.js
@@ -28,129 +28,129 @@ var should = require('should'),
 	moment = require('moment');
 
 describe('romcal', function() {
-    // describe('calendarFor(): no arguments', function() {
-    //     it('returns an array of current year dates of either 365 or 366 days', function( done ) {
-    //         romcal.calendarFor( function( dates ) {
-    //             dates.length.should.be.greaterThan(364);
-    //             dates.length.should.be.lessThan(367);
-    //             done();
-    //         });
-    //     });
-    // });
-    // describe('calendarFor("2007"): get all dates in the Gregorian year 2008', function() {
-    //     it('returns an array of 2007 dates', function( done ) {
-    //         romcal.calendarFor('2007', function( dates ) {
-    //             lodash.map( dates, function( v, k ) {
-    //                 console.log( v.moment.toString(), ':', v.data.nationalCalendar );
-    //                 v.moment.year().should.be.eql(2007);
-    //             });
-    //             done();
-    //         });
-    //     });
-    // });
-    // describe('queryNationalCalendar("2008", "es-AR", "argentina"): get the Argentinian National Calendar, locale es-AR for 2008', function() {
-    //     it('returns an array of dates for the Argentinian National Calendar, 2008', function( done ) {
-    //         romcal.queryNationalCalendar('2008', 'es-AR', 'argentina', function ( err, dates ) {
-    //             dates.length.should.be.greaterThan(364);
-    //             dates.length.should.be.lessThan(367);
-    //             lodash.map( dates, function( v, k ) {
-    //                 v.moment.year().should.be.eql(2008);
-    //             });
-    //             done();
-    //         });
-    //     });
-    // });
-    describe('queryNationalCalendar("2008", "en-US", "unitedStates"): get the National Calendar of the US, locale en-US for 2008', function() {
-        it('returns an array of dates for the National Calendar for the US, 2008', function( done ) {
-            romcal.queryNationalCalendar('2008', 'en-US', 'unitedStates', function ( err, dates ) {
+    describe('calendarFor(): no arguments', function() {
+        it('returns an array of current year dates of either 365 or 366 days', function( done ) {
+            romcal.calendarFor( function( dates ) {
+                dates.length.should.be.greaterThan(364);
+                dates.length.should.be.lessThan(367);
+                done();
+            });
+        });
+    });
+    describe('calendarFor("2007"): get all dates in the Gregorian year 2008', function() {
+        it('returns an array of 2007 dates', function( done ) {
+            romcal.calendarFor('2007', function( dates ) {
+                lodash.map( dates, function( v, k ) {
+                    console.log( v.moment.toString(), ':', v.data.nationalCalendar );
+                    v.moment.year().should.be.eql(2007);
+                });
+                done();
+            });
+        });
+    });
+    describe('queryNationalCalendar("2008", "es-AR", "argentina"): get the Argentinian National Calendar, locale es-AR for 2008', function() {
+        it('returns an array of dates for the Argentinian National Calendar, 2008', function( done ) {
+            romcal.queryNationalCalendar('2008', 'es-AR', 'argentina', function ( err, dates ) {
                 dates.length.should.be.greaterThan(364);
                 dates.length.should.be.lessThan(367);
                 lodash.map( dates, function( v, k ) {
-                    console.log( v.moment.toString(), ':', v.name, '-', v.type.name );
                     v.moment.year().should.be.eql(2008);
                 });
                 done();
             });
         });
     });
-    // describe('Query Thursdays in the US National Calendar, locale en-US for 2008', function() {
-    //     it('returns an array of Thursdays for the US National Calendar, locale en-US, 2008', function( done ) {
-    //         romcal.queryNationalCalendar('2008', 'en-US', 'unitedStates', function ( err, dates ) {
-    //             if ( err ) throw new Error( err );
-    //             romcal.queryFor('thursdays', dates, function( errz, query ) {
-    //                 lodash.map( query, function( v, k ) {
-    //                     if ( errz ) throw new Error( errz );
-    //                     console.log( v.moment.toString(), ':', v.name, '-', v.type.name );
-    //                     v.moment.day().should.be.eql(4);
-    //                 });
-    //                 done(); 
-    //             });              
-    //         });
-    //     });
-    // });
-    // describe('queryFor("january", dates ): query all dates in January 2012', function() {
-    //     it('returns an array of dates in January 2012 with length 31', function( done ) {
-    //         romcal.calendarFor('2012', function( dates ) {
-    //             romcal.queryFor('january', dates, function( err, query ) {
-    //                 if ( err ) {
-    //                     throw new Error( err );
-    //                     return;
-    //                 }
-    //                 lodash.map( query, function( v, k ) {
-    //                     v.moment.month().should.be.eql(0);
-    //                 });
-    //                 query.length.should.be.eql(31);
-    //                 done();
-    //             });
-    //         });
-    //     });
-    // });
-    // describe('queryFor("mondays", dates ): query all dates that are Mondays in 2013', function() {
-    //     it('returns an array of dates falling on Monday in 2013', function( done ) {
-    //         romcal.calendarFor('2013', function( dates ) {
-    //             romcal.queryFor('mondays', dates, function( err, query ) {
-    //                 if ( err ) {
-    //                     throw new Error( err );
-    //                     return;
-    //                 }
-    //                 lodash.map( query, function( v, k ) {
-    //                     v.moment.day().should.be.eql(1);
-    //                 });
-    //                 done();
-    //             });
-    //         });
-    //     });
-    // });
-    // describe('queryFor("solemnities", dates ): query all dates that are Solemnities in 2009', function() {
-    //     it('returns an array of dates that are Solemnities in 2009', function( done ) {
-    //         var dates = romcal.calendarFor('2009', function( dates ) {
-    //             romcal.queryFor('solemnities', dates, function( err, query ) {
-    //                 if ( err ) {
-    //                     throw new Error( err );
-    //                     return;
-    //                 }
-    //                 lodash.map( query, function( v, k ) {
-    //                     v.type.id.should.be.eql('SOLEMNITY');
-    //                 });
-    //                 done();
-    //             });
-    //         });
-    //     });
-    // });
-    // describe('queryFor("holyweek", dates ): query all dates that are within Holy Week, 2011', function() {
-    //     it('returns an array of dates that are within Holy Week, 2011', function( done ) {
-    //         var dates = romcal.calendarFor('2011', function( dates ) {
-    //             romcal.queryFor('holyWeek', dates, function( err, query ) {
-    //                 if ( err ) {
-    //                     throw new Error( err );
-    //                     return;
-    //                 }
-    //                 lodash.map( query, function( v, k ) {
-    //                     v.data.season.should.be.eql('HolyWeek');
-    //                 });
-    //                 done();
-    //             });
-    //         });
-    //     });
-    // });
+    describe('queryNationalCalendar("2008", "en-US", "unitedStates"): get the National Calendar of the US, locale en-US for 2008', function() {
+        it('returns an array of dates for the National Calendar for the US, 2008', function( done ) {
+            romcal.queryNationalCalendar('2008', 'en-US', 'unitedStates', function ( err, dates ) {
+                dates.length.should.be.greaterThan(364);
+                dates.length.should.be.lessThan(367);
+                lodash.map( dates, function( v, k ) {
+                    // console.log( v.moment.toString(), ':', v.name, '-', v.type.name );
+                    v.moment.year().should.be.eql(2008);
+                });
+                done();
+            });
+        });
+    });
+    describe('Query Thursdays in the US National Calendar, locale en-US for 2008', function() {
+        it('returns an array of Thursdays for the US National Calendar, locale en-US, 2008', function( done ) {
+            romcal.queryNationalCalendar('2008', 'en-US', 'unitedStates', function ( err, dates ) {
+                if ( err ) throw new Error( err );
+                romcal.queryFor('thursdays', dates, function( errz, query ) {
+                    lodash.map( query, function( v, k ) {
+                        if ( errz ) throw new Error( errz );
+                        console.log( v.moment.toString(), ':', v.name, '-', v.type.name );
+                        v.moment.day().should.be.eql(4);
+                    });
+                    done(); 
+                });              
+            });
+        });
+    });
+    describe('queryFor("january", dates ): query all dates in January 2012', function() {
+        it('returns an array of dates in January 2012 with length 31', function( done ) {
+            romcal.calendarFor('2012', function( dates ) {
+                romcal.queryFor('january', dates, function( err, query ) {
+                    if ( err ) {
+                        throw new Error( err );
+                        return;
+                    }
+                    lodash.map( query, function( v, k ) {
+                        v.moment.month().should.be.eql(0);
+                    });
+                    query.length.should.be.eql(31);
+                    done();
+                });
+            });
+        });
+    });
+    describe('queryFor("mondays", dates ): query all dates that are Mondays in 2013', function() {
+        it('returns an array of dates falling on Monday in 2013', function( done ) {
+            romcal.calendarFor('2013', function( dates ) {
+                romcal.queryFor('mondays', dates, function( err, query ) {
+                    if ( err ) {
+                        throw new Error( err );
+                        return;
+                    }
+                    lodash.map( query, function( v, k ) {
+                        v.moment.day().should.be.eql(1);
+                    });
+                    done();
+                });
+            });
+        });
+    });
+    describe('queryFor("solemnities", dates ): query all dates that are Solemnities in 2009', function() {
+        it('returns an array of dates that are Solemnities in 2009', function( done ) {
+            var dates = romcal.calendarFor('2009', function( dates ) {
+                romcal.queryFor('solemnities', dates, function( err, query ) {
+                    if ( err ) {
+                        throw new Error( err );
+                        return;
+                    }
+                    lodash.map( query, function( v, k ) {
+                        v.type.id.should.be.eql('SOLEMNITY');
+                    });
+                    done();
+                });
+            });
+        });
+    });
+    describe('queryFor("holyweek", dates ): query all dates that are within Holy Week, 2011', function() {
+        it('returns an array of dates that are within Holy Week, 2011', function( done ) {
+            var dates = romcal.calendarFor('2011', function( dates ) {
+                romcal.queryFor('holyWeek', dates, function( err, query ) {
+                    if ( err ) {
+                        throw new Error( err );
+                        return;
+                    }
+                    lodash.map( query, function( v, k ) {
+                        v.data.season.should.be.eql('HolyWeek');
+                    });
+                    done();
+                });
+            });
+        });
+    });
 });
