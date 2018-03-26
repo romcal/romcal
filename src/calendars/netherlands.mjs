ea5ddeadad08b82192e7093096249d6c63962650
Julian Pereira <bleushade@gmail.com>
2018-03-26 13:42:55 +0800

    issue #31: new country calendars for European countries

---
+++ src/calendars/netherlands.mjs
@@ -0,0 +1,28 @@
+import moment from 'moment';
+import _ from 'lodash';
+
+import { Dates, Utils } from '../lib';
+import { Titles, Types, LiturgicalColors } from '../constants';
+
+let dates = year => {
+
+  let _dates = [
+    {
+      "key": "saintBridgetOfSwedenReligious",
+      "type": Types[4],
+      "moment": moment.utc({ year: year, month: 6, day: 23 }),
+      "data": {
+        "meta": {
+          "liturgicalColor": LiturgicalColors.WHITE
+        }
+      }
+    }
+  ];
+
+  // Get localized celebration names
+  return Utils.localizeDates(_dates);
+};
+
+export {
+  dates
+};
