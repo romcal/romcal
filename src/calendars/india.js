4fc0b965f02480a9fd9c1e469411addc5874c0fb
Etienne <etienne.magnier@gmail.com>
2020-01-26 21:48:19 +0100

    Add defaultConfig preset on all calendars

--- src/calendars/india.js
+++ src/calendars/india.js
@@ -4,6 +4,8 @@ import _ from 'lodash';
 import { Dates, Utils } from '../lib';
 import { Titles, Types, LiturgicalColors } from '../constants';
 
+const defaultConfig = {};
+
 let dates = year => {
 
   let _dates = [
@@ -98,5 +100,6 @@ let dates = year => {
 };
 
 export {
+  defaultConfig,
   dates
 };
--- src/calendars/russia.js
+++ src/calendars/russia.js
@@ -4,6 +4,8 @@ import _ from 'lodash';
 import { Dates, Utils } from '../lib';
 import { Titles, Types, LiturgicalColors } from '../constants';
 
+const defaultConfig = {};
+
 let dates = year => {
 
   let _dates = [
@@ -234,5 +236,6 @@ let dates = year => {
 };
 
 export {
+  defaultConfig,
   dates
 };
