d07c6138497e5448e90c8012ae79540eb15b62c3
Julian Pereira <bleushade@gmail.com>
2020-02-21 00:25:35 +0800

    task: bugs in remove code

--- src/utils/array.test.ts
+++ src/utils/array.test.ts
@@ -1,4 +1,4 @@
-import { concatAll } from "./array";
+import { concatAll, removeWhere } from "./array";
 
 describe("concatAll", () => {
     test("should flatten and merge array item", () => {
@@ -8,5 +8,33 @@ describe("concatAll", () => {
     test("should flatten and merge array item WITHOUT removing duplicates", () => {
         const result = concatAll([1, 2, [1, 2], [3, 4, 5]]);
         expect(result).toStrictEqual([1, 2, 1, 2, 3, 4, 5]);
+    });
+});
+
+describe("removeWhere", () => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    let items: any;
+    beforeEach(() => {
+        items = [
+            {
+                id: 1,
+                name: "foo",
+            },
+            {
+                id: 2,
+                name: "bar",
+            },
+        ];
+    });
+    test("should remove objects from array with matching keys and values", () => {
+        removeWhere(items, { id: 1 });
+        expect(items).toStrictEqual([{ id: 2, name: "bar" }]);
+    });
+    test("should return the original array if asked to delete an item that doesn't exist", () => {
+        removeWhere(items, { id: 3 });
+        expect(items).toStrictEqual([
+            { id: 1, name: "foo" },
+            { id: 2, name: "bar" },
+        ]);
     });
 });
