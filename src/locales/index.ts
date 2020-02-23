d301865cf022b98a2f44d2b634b44885e577666e
Julian Pereira <bleushade@gmail.com>
2020-02-23 13:25:42 +0800

    task: fix currentLocale issue on example HTML

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,8 +1,9 @@
 import enCA from "./en-CA";
+import enUS from "./en-US";
 import en from "./en";
 import fr from "./fr";
 import it from "./it";
 import pl from "./pl";
 import sk from "./sk";
 
-export { enCA, en, fr, it, pl, sk };
+export { enCA, enUS, en, fr, it, pl, sk };
