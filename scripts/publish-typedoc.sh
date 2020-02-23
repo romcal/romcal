4ad9c1bc52d8f2f575541f6c15a5a80e4610525a
Julian Pereira <bleushade@gmail.com>
2020-02-23 13:48:18 +0800

    task: add link for typedoc

--- scripts/publish-typedoc.sh
+++ scripts/publish-typedoc.sh
@@ -3,10 +3,19 @@
 TRAVIS_BRANCH=$1
 echo "TRAVIS_BRANCH IS ${TRAVIS_BRANCH}"
 
+if [ "$TRAVIS_BRANCH" != 'master' ]; then
+    LATEST_TAG_VERSION="$(npm view ${PACKAGE_NAME}@latest version)"
+elif [ "$TRAVIS_BRANCH" = 'test' ]; then
+    LATEST_TAG_VERSION="$(npm view ${PACKAGE_NAME}@beta version)"
+elif [ "$TRAVIS_BRANCH" = 'dev' ]; then
+    LATEST_TAG_VERSION="$(npm view ${PACKAGE_NAME}@alpha version)"
+else
+    LATEST_TAG_VERSION="$(npm view ${PACKAGE_NAME}@canary version)"
+fi
+
 # Create the docs folder with generated API docs
 npm run typedoc
 
 # gh-pages
-git add docs && git commit -am "[skip travis-ci] Generate latest typedoc"
+git add docs && git commit -am "[skip travis-ci] Publish latest typedoc for ${LATEST_TAG_VERSION}"
 git subtree push --prefix docs origin gh-pages
-
