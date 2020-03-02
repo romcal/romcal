9c685ef969a5be018cd87913dd77de38cec0cbd2
Julian Pereira <bleushade@gmail.com>
2020-03-03 00:05:49 +0800

    [skip travis-ci] WIP differential build setup

--- __tests__/external_user.test.ts
+++
@@ -1,7 +0,0 @@
-import { calendarFor } from "../dist/index";
-
-describe.skip("test me", () => {
-    test("hello", () => {
-        console.log(calendarFor);
-    });
-});
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,72 +1,21 @@
 import { join } from "path";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
-import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import { ConfigurationFactory } from "webpack";
 
 // https://webpack.js.org/configuration/configuration-languages/#typescript
-const config: ConfigurationFactory = (env, { mode }) => ({
+const baseConfiguration: ConfigurationFactory = (env, { mode }) => ({
     devtool: mode === "production" ? "source-map" : "inline-source-map",
 
     entry: {
         romcal: join(__dirname, "./src/index.ts"),
     },
 
-    output: {
-        filename: "[name].bundle.js",
-        chunkFilename: "[name].bundle.js",
-        path: join(__dirname, "dist"),
-        library: "romcal",
-        libraryTarget: "umd",
-        auxiliaryComment: "romcal - The Liturgical Calendar generator",
-    },
-
     resolve: {
         extensions: [".ts", ".js", ".json"],
     },
 
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                loader: "babel-loader",
-                exclude: [/node_modules/, "/src/**/*.test.ts"],
-            },
-            {
-                test: /\.html$/,
-                loader: "html-loader",
-            },
-        ],
-    },
-
-    optimization: {
-        splitChunks: {
-            chunks: "all",
-            maxInitialRequests: Infinity,
-            minSize: 0,
-            cacheGroups: {
-                // Externalize all includes from node_modules except core-js
-                vendor: {
-                    chunks: "all",
-                    test: /[\\/]node_modules[\\/]/,
-                    priority: 20,
-                },
-                // Create a common chunk for reusable code
-                common: {
-                    name: "common",
-                    minChunks: 2,
-                    chunks: "async",
-                    priority: 10,
-                    reuseExistingChunk: true,
-                    enforce: true,
-                },
-            },
-        },
-    },
-
     plugins: [
-        // Clean the dist folder
-        new CleanWebpackPlugin(),
         // Generate test HTML page
         new HtmlWebpackPlugin({
             hash: true,
@@ -98,4 +47,4 @@ const config: ConfigurationFactory = (env, { mode }) => ({
     externals: {},
 });
 
-export default config;
+export default baseConfiguration;
