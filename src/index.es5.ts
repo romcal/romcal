9b8b61109a5b39bdf734786f7355a1cdb690e54d
Julian Pereira <bleushade@gmail.com>
2020-03-03 09:51:38 +0800

    [skip travis-ci] almost there with differential builds

---
+++ src/index.es5.ts
@@ -0,0 +1,7 @@
+/**
+ * For Babel to transpile browser polyfills and such...
+ * Do not add any other code here!
+ * Use index.ts for other code.
+ */
+import "core-js/stable";
+import "regenerator-runtime/runtime";
