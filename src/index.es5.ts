df34dd40bc6fbc9aff948f0dd45b26eec6fcf0fa
Julian Pereira <bleushade@gmail.com>
2020-03-07 17:47:32 +0800

    task: fix improper implementation of @babel/preset-env that caused unnecessary bloat

--- src/index.es5.ts
+++
@@ -1,7 +0,0 @@
-/**
- * For Babel to transpile browser polyfills and such...
- * Do not add any other code here!
- * Use index.ts for other code.
- */
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
