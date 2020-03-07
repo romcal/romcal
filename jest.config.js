079e28239720f4a4c811c30c9c0ac8a7173ecc11
Julian Pereira <bleushade@gmail.com>
2020-03-07 17:47:32 +0800

    task: fix improper implementation of @babel/preset-env that caused unnecessary bloat

--- jest.config.js
+++ jest.config.js
@@ -1,31 +1,31 @@
 module.exports = {
-    transform: {
-        "^.+\\.(j|t)sx?$": "ts-jest",
-        ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
+  transform: {
+    '^.+\\.(j|t)sx?$': 'ts-jest',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+  },
+  transformIgnorePatterns: ['<rootDir>/node_modules/(?!lodash-es/.*)'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.test{ts,tsx}'],
+  coveragePathIgnorePatterns: ['/node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
     },
-    transformIgnorePatterns: ["<rootDir>/node_modules/(?!lodash-es/.*)"],
-    testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
-    moduleFileExtensions: ["ts", "tsx", "js", "json", "node"],
-    // collectCoverage: true,
-    collectCoverageFrom: ["src/**/*.{ts,tsx}", "!src/**/*.test{ts,tsx}"],
-    coveragePathIgnorePatterns: ["/node_modules/"],
-    coverageThreshold: {
-        global: {
-            branches: 70,
-            functions: 70,
-            lines: 70,
-            statements: 70,
-        },
+  },
+  moduleNameMapper: {
+    '^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+  },
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json',
+      diagnostics: true,
     },
-    moduleNameMapper: {
-        "^.+.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
-    },
-    globals: {
-        "ts-jest": {
-            tsConfig: "tsconfig.json",
-            diagnostics: true,
-        },
-    },
-    setupFilesAfterEnv: ["jest-extended"],
-    verbose: true,
+  },
+  setupFilesAfterEnv: ['jest-extended'],
+  verbose: true,
 };
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,193 +1,194 @@
-import { MultiConfigurationFactory } from "webpack";
-import webpack = require("webpack");
-import { join } from "path";
-import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { MultiConfigurationFactory } from 'webpack';
+import webpack = require('webpack');
+import { join } from 'path';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const getDevTool = (mode: string): webpack.Options.Devtool =>
-    mode === "production" ? "source-map" : "inline-source-map";
+  mode === 'production' ? 'source-map' : 'inline-source-map';
 
 const getResolveExtensions = (): webpack.Resolve => ({
-    extensions: [".ts", ".js", ".json"],
+  extensions: ['.ts', '.js', '.json'],
 });
 
 const getEntryPoints = (): string[] => {
-    return [join(__dirname, "src/index.ts")];
+  return [join(__dirname, 'src/index.ts')];
 };
 
 const getWebpackOutput = (subpath: string): webpack.Output => ({
-    path: join(__dirname, `/dist/${subpath}`),
+  path: join(__dirname, `/dist/${subpath}`),
 });
 
 const getTsLoaderRuleSet = (tsConfigFilePath: string): webpack.RuleSetUseItem => ({
-    loader: "ts-loader",
-    options: {
-        configFile: join(__dirname, tsConfigFilePath),
-        colors: true,
-    },
+  loader: 'ts-loader',
+  options: {
+    configFile: join(__dirname, tsConfigFilePath),
+    colors: true,
+  },
 });
 
 const getBabelRuleSetForEs5 = (): webpack.RuleSetUseItem => ({
-    loader: "babel-loader",
-    options: {
-        presets: [
-            [
-                "@babel/preset-env",
-                {
-                    useBuiltIns: "usage",
-                    corejs: "3",
-                    debug: false,
-                    ignoreBrowserslistConfig: true,
-                    targets: {
-                        browsers: [
-                            ">= 1%",
-                            "last 1 major version",
-                            "not dead",
-                            "Chrome >= 45",
-                            "Firefox >= 38",
-                            "Edge >= 12",
-                            "Explorer >= 10",
-                            "iOS >= 9",
-                            "Safari >= 9",
-                            "Android >= 4.4",
-                            "Opera >= 30",
-                        ],
-                    },
-                },
+  loader: 'babel-loader',
+  options: {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: '3',
+          debug: true,
+          ignoreBrowserslistConfig: true,
+          targets: {
+            browsers: [
+              '>= 1%',
+              'last 1 major version',
+              'not dead',
+              'Chrome >= 45',
+              'Firefox >= 38',
+              'Edge >= 12',
+              'Explorer >= 10',
+              'iOS >= 9',
+              'Safari >= 9',
+              'Android >= 4.4',
+              'Opera >= 30',
             ],
-        ],
-    },
+          },
+        },
+      ],
+    ],
+  },
 });
 
 const getHtmlLoader = (): webpack.RuleSetRule => ({
-    test: /\.html$/,
-    loader: "html-loader",
+  test: /\.html$/,
+  loader: 'html-loader',
 });
 
 const getOptimizationEs5 = (): webpack.Options.Optimization => ({
-    splitChunks: {
-        chunks: "all",
-        cacheGroups: {
-            // Externalize all includes from node_modules
-            vendor: {
-                test: /[\\/]node_modules[\\/]/,
-                enforce: true,
-            },
-        },
+  splitChunks: {
+    chunks: 'all',
+    cacheGroups: {
+      // Externalize all includes from node_modules
+      vendor: {
+        test: /[\\/]node_modules[\\/]/,
+        enforce: true,
+      },
     },
+  },
 });
 
 /**
  * Analyze bundle size
  */
 const getBundleAnalyzerPlugin = (): BundleAnalyzerPlugin => {
-    const bundleAnalyzerPlugin = new BundleAnalyzerPlugin({
-        analyzerMode: "disabled",
-        generateStatsFile: true,
-    });
-    return bundleAnalyzerPlugin;
+  const bundleAnalyzerPlugin = new BundleAnalyzerPlugin({
+    analyzerMode: 'disabled',
+    generateStatsFile: true,
+  });
+  return bundleAnalyzerPlugin;
 };
 
 const getHtmlWebpackPlugin = (): HtmlWebpackPlugin => {
-    const htmlWebpackPlugin = new HtmlWebpackPlugin({
-        hash: true,
-        title: "romcal - Test Page",
-        template: "src/index.html",
-        base: "./dist",
-        meta: {
-            charset: {
-                charset: "utf-8",
-            },
-            "X-UA-Compatible": {
-                "http-equiv": "X-UA-Compatible",
-                content: "IE=edge",
-            },
-        },
-        minify: false,
-    });
-    return htmlWebpackPlugin;
+  const htmlWebpackPlugin = new HtmlWebpackPlugin({
+    hash: true,
+    title: 'romcal - Test Page',
+    template: 'src/index.html',
+    base: './dist',
+    meta: {
+      charset: {
+        charset: 'utf-8',
+      },
+      'X-UA-Compatible': {
+        'http-equiv': 'X-UA-Compatible',
+        content: 'IE=edge',
+      },
+    },
+    minify: false,
+  });
+  return htmlWebpackPlugin;
 };
 
 const configurations: MultiConfigurationFactory = (env, { mode }) => [
-    // ES5
-    {
-        devtool: getDevTool(mode),
-        resolve: getResolveExtensions(),
+  // ES5
+  {
+    devtool: getDevTool(mode),
+    resolve: getResolveExtensions(),
 
-        entry: {
-            romcal: [join(__dirname, "src/index.es5.ts"), ...getEntryPoints()],
-        },
-
-        output: {
-            ...getWebpackOutput("es5"),
-            filename: "[name].bundle.es5.js",
-            chunkFilename: "[name].bundle.es5.js",
-            crossOriginLoading: "anonymous",
-            libraryTarget: "umd",
-            libraryExport: "default",
-            library: "romcal",
-            jsonpFunction: "romcalWebpackJsonpFunction",
-            globalObject: "window",
-            auxiliaryComment: "romcal - The Liturgical Calendar generator",
-        },
-
-        module: {
-            rules: [
-                getHtmlLoader(),
-                {
-                    test: /\.ts(x?)$/,
-                    exclude: [/node_modules/, "/src/**/*.test.ts"],
-                    use: [getBabelRuleSetForEs5(), getTsLoaderRuleSet("tsconfig.json")],
-                },
-            ],
-        },
-
-        optimization: getOptimizationEs5(),
-
-        plugins: [
-            new webpack.DefinePlugin({
-                "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-                "process.env.BUILD_TYPE": JSON.stringify(`ES5`),
-            }),
-            getBundleAnalyzerPlugin(),
-            getHtmlWebpackPlugin(),
-        ],
+    entry: {
+      romcal: [...getEntryPoints()],
     },
-    // ESM
-    {
-        devtool: getDevTool(mode),
-        resolve: getResolveExtensions(),
 
-        entry: {
-            romcal: [...getEntryPoints()],
-        },
-
-        output: {
-            ...getWebpackOutput("esm"),
-            filename: "index.js",
-            chunkFilename: "[name].js",
-            libraryTarget: "umd",
-            globalObject: "this",
-            publicPath: "/dist/esm/",
-        },
-
-        plugins: [
-            new webpack.DefinePlugin({
-                "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
-                "process.env.BUILD_TYPE": JSON.stringify(`ESM`),
-            }),
-        ],
-
-        module: {
-            rules: [
-                {
-                    test: /\.ts(x?)$/,
-                    exclude: [/node_modules/, "/src/**/*.test.ts"],
-                    use: [getTsLoaderRuleSet("tsconfig.json")],
-                },
-            ],
-        },
+    output: {
+      ...getWebpackOutput('es5'),
+      filename: '[name].bundle.es5.js',
+      chunkFilename: '[name].bundle.es5.js',
+      crossOriginLoading: 'anonymous',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      library: 'romcal',
+      jsonpFunction: 'romcalWebpackJsonpFunction',
+      globalObject: 'window',
+      auxiliaryComment: 'romcal - The Liturgical Calendar generator',
     },
+
+    module: {
+      rules: [
+        getHtmlLoader(),
+        {
+          test: /\.ts(x?)$/,
+          exclude: [/node_modules/, '/src/**/*.test.ts'],
+          use: [getBabelRuleSetForEs5(), getTsLoaderRuleSet('tsconfig.json')],
+        },
+      ],
+    },
+
+    optimization: getOptimizationEs5(),
+
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.BUILD_TYPE': JSON.stringify('ES5'),
+      }),
+      getBundleAnalyzerPlugin(),
+      getHtmlWebpackPlugin(),
+    ],
+  },
+  // ESM
+  {
+    devtool: getDevTool(mode),
+    resolve: getResolveExtensions(),
+
+    entry: {
+      romcal: [...getEntryPoints()],
+    },
+
+    output: {
+      ...getWebpackOutput('esm'),
+      filename: 'index.js',
+      chunkFilename: '[name].js',
+      libraryTarget: 'umd',
+      globalObject: 'this',
+      publicPath: '/dist/esm/',
+    },
+
+    plugins: [
+      getBundleAnalyzerPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'process.env.BUILD_TYPE': JSON.stringify('ESM'),
+      }),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.ts(x?)$/,
+          exclude: [/node_modules/, '/src/**/*.test.ts'],
+          use: [getTsLoaderRuleSet('tsconfig.json')],
+        },
+      ],
+    },
+  },
 ];
 
 export default configurations;
