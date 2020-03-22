ac35cdc2c38a3b01005f4928239866e8bdcbd678
Henrique Derosa <contato.h@gmail.com>
2020-03-22 08:38:51 -0300

    Add locale for brazilian portuguese (#189)
    * feat: initial commit for brazilian portuguese locale
    
    * fix: advent feria
    
    Co-authored-by: Henrique Derosa <henriquederosa@Henriques-MacBook-Pro.local>

---
+++ src/locales/pt-BR.js
@@ -0,0 +1,95 @@
+export default {
+  "advent": {
+    "season": "Advento",
+    "feria": "{{day}} da {{week}} semana do Advento",
+    "sunday": "{{week}} Domingo do Advento"
+  },
+  "christmastide": {
+    "season": "Natal",
+    "day": "{{day}} do Tempo do Natal",
+    "octave": "{{count}} dia da Oitava de Natal",
+    "sunday": "{{count}} Domingo do Natal"
+  },
+  "epiphany": {
+    "season": "Epifania do Senhor",
+    "before": "{{day}} antes da Epifania do Senhor",
+    "after": "{{day}} depois da Epifania do Senhor"
+  },
+  "ordinaryTime": {
+    "season": "Tempo Comum",
+    "feria": "{{day}} da {{week}} semana do Tempo Comum",
+    "sunday": "{{week}} Domingo do Tempo Comum"
+  },
+  "lent": {
+    "season": "Quaresma",
+    "feria": "{{day}} da {{week}} semana da Quaresma",
+    "sunday": "{{week}} Domingo da Quaresma",
+    "day_after_ash_wed": "{{day}} depois da Quarta-feira de Cinzas"
+  },
+  "holyWeek": {
+    "season": "Semana Santa",
+    "feria": "{{day}} of Semana Santa"
+  },
+  "eastertide": {
+    "season": "Páscoa",
+    "feria": "{{day}} da {{week}} semana do Tempo Pascal",
+    "sunday": "{{week}} Domingo do Tempo Pascal",
+    "octave": "Tempo Pascal {{day}}"
+  },
+  "celebrations": {
+    "allSaints": "Todos os Santos",
+    "annunciation": "Anunciação do Senhor",
+    "ascension": "Ascensão do Senhor",
+    "ashWednesday": "Quarta-feira de Cinzas",
+    "assumption": "Assunção da Bem Aventurada Virgem Maria",
+    "baptismOfTheLord": "Batismo do Senhor",
+    "birthOfJohnTheBaptist": "Nascimento de João Batista",
+    "christmas": "Natal",
+    "christTheKing": "Cristo Rei do Universo",
+    "corpusChristi": "Corpus Christi",
+    "divineMercySunday": "Domingo da Misericórdia",
+    "easter": "Domingo da Páscoa",
+    "epiphany": "Epifania do Senhor",
+    "goodFriday": "Sexta-feira Santa",
+    "holyFamily": "Sagrada Família",
+    "holySaturday": "Sábado Santo",
+    "holyThursday": "Quinta-feira Santa",
+    "immaculateConception": "Imaculada Conceição",
+    "immaculateHeartOfMary": "Imaculado Coração de Maria",
+    "josephHusbandOfMary": "São José, esposo de Maria",
+    "maryMotherOfGod": "Maria, Mãe de Deus",
+    "palmSunday": "Domingo de Ramos",
+    "pentecostSunday": "Pentecostes",
+    "peterAndPaulApostles": "São Pedro e São Paulo, Apóstolos",
+    "presentationOfTheLord": "Apresentação do Senhor",
+    "sacredHeartOfJesus": "Sagrado Coração de Jesus",
+    "theExaltationOfTheHolyCross": "Exaltação da Santa Cruz",
+    "transfiguration": "Transfiguração do Senhor",
+    "trinitySunday": "Santíssima Trindade"
+  },
+  "sanctoral": {
+    "ourLadyHelpOfChristians": "Nossa Senhora, Auxílio dos Cristãos",
+    "ourLadyMediatrix": "Nossa Senhora, Medianeira",
+    "ourLadyMediatrixOfAllGrace": "Nossa Senhora, medianeira de todas as Graças",
+    "ourLadyMotherOfDivineProvidencePatronessOfPuertoRico": "Nossa Senhora, Mãe da Divina Providência",
+    "ourLadyMotherOfMercy": "Nossa Senhora, Mãe de Misericórdia",
+    "ourLadyOfAparecidaPatronessOfBrazil": "Nossa Senhora Aparecida",
+    "ourLadyOfChina": "Nossa Senhora da China",
+    "ourLadyOfFatima": "Nossa Senhora de Fatima",
+    "ourLadyOfGoodCounsel": "Nossa Senhora do Bom Conselho",
+    "ourLadyOfGuadalupe": "Nossa Senhora de Guadalupe",
+    "ourLadyOfHungary": "Nossa Senhora da Hungria",
+    "ourLadyOfLoreto": "Nossa Senhora de Loreto",
+    "ourLadyOfLourdes": "Nossa Senhora de Lourdes",
+    "ourLadyOfLujanPatronessOfArgentina": "Nossa Senhora de Luján, padroeira da Argentina",
+    "ourLadyOfMountCarmel": "Nossa Senhora do Monte Carmelo",
+    "ourLadyOfPeace": "Nossa Senhora da Paz",
+    "ourLadyOfPerpetualHelpOrBlessedLeonidFeodorovPriestAndMartyr": "Nossa Senhora do Perpétuo Socorro",
+    "ourLadyOfThePillar": "Nossa Senhora do Pilar",
+    "ourLadyOfTheRosary": "Nossa Senhora do Rosário",
+    "ourLadyQueenOfPeace": "Nossa Senhora, Rainha da Paz",
+    "popeSaintJohnPaulII": "Papa São João Paulo II",
+    "popeSaintJohnXXIII": "Papa São João XXIII",
+    "presentationOfTheBlessedVirginMary": "Apresentação da Virgem Maria",
+  }
+};
