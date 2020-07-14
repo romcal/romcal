7638f3e58c98e6c0c04038f668209e7d9ad51c0a
Alejandro Ulate <alejandro.ulate@buildto.io>
2020-07-14 13:07:26 -0600

    Changelog: - Added spanish locale.

--- src/locales/index.js
+++ src/locales/index.js
@@ -5,6 +5,7 @@ import it from './it';
 import pl from './pl';
 import sk from './sk';
 import ptBR from './pt-BR';
+import es from './es';
 
 export {
   enCA,
@@ -14,4 +15,5 @@ export {
   pl,
   sk,
   ptBR,
+  es,
 };
