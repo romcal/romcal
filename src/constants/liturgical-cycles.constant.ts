bcbecb8ef89b0d9f9870aad8187d1fd10db8095b
Etienne <etienne.magnier@gmail.com>
2020-08-07 00:21:37 +0200

    task: refactor output schema: psalter weeks cycle

--- src/constants/liturgical-cycles.constant.ts
+++ src/constants/liturgical-cycles.constant.ts
@@ -1,2 +1,3 @@
 export const LITURGICAL_SUNDAY_CYCLES = ['A', 'B', 'C'] as const;
 export const LITURGICAL_FERIAL_CYCLES = [1, 2] as const;
+export const PSALTER_WEEKS = ['WEEK_1', 'WEEK_2', 'WEEK_3', 'WEEK_4'] as const;
