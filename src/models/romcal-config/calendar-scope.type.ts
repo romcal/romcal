c426efb9c8215d51226822d68219714726d2f1f7
Etienne <etienne.magnier@gmail.com>
2020-08-13 18:23:09 +0200

    task: rename 'calendar' to 'gregorian' (scope type) + doc refactor

--- src/models/romcal-config/calendar-scope.type.ts
+++ src/models/romcal-config/calendar-scope.type.ts
@@ -1,4 +1,4 @@
 /**
  * The calendar scope that can be resolved by this library.
  */
-export type RomcalCalendarScope = 'calendar' | 'liturgical';
+export type RomcalCalendarScope = 'gregorian' | 'liturgical';
