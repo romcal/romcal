af3575b8b711d2308d3fb123852fea75a10d70c7
Etienne Magnier <etienne.magnier@gmail.com>
2021-05-19 11:37:50 +0200

    docs: remove doc duplicate about RomcalLiturgicalDayNamespace

--- src/models/liturgical-day/liturgical-day.types.ts
+++ src/models/liturgical-day/liturgical-day.types.ts
@@ -196,13 +196,6 @@ export type LiturgicalDayMetadata = {
 
 /**
  * The locale namespace of the liturgical day.
- *
- * This value is used when localizing dates so that the [[Locales.localizeDates]] function knows
- * which subtree in the locale file to look for.
- *
- * A special key, "temporale", may be used when you do not wish to specify any subtree but rather
- * provide the entire path (as a period delimited string) to the [[Locales.localizeDates]] function
- * to lookup.
  */
 export type RomcalLiturgicalDayNamespace =
   | 'temporale'
