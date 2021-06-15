86a6656d06c9c601ec55eba5f8b6f9a7bb078cc3
Etienne Magnier <etienne.magnier@gmail.com>
2021-06-15 13:00:55 +0200

    feat: add cycles and calendar metadata

--- lib/rites/roman/constants/cycles.ts
+++ lib/rites/roman/constants/cycles.ts
@@ -1,7 +1,7 @@
 /**
  * Liturgical Day cycle that can be used as metadata for liturgical days.
  */
-export enum LiturgicalDayCycle {
+export enum LiturgicalDayCycles {
   TEMPORALE = 'TEMPORALE',
   SANCTORALE = 'SANCTORALE',
 }
@@ -9,7 +9,7 @@ export enum LiturgicalDayCycle {
 /**
  * Sundays cycle that can be used as metadata for liturgical days.
  */
-export enum SundaysCycle {
+export enum SundaysCycles {
   YEAR_A = 'YEAR_A',
   YEAR_B = 'YEAR_B',
   YEAR_C = 'YEAR_C',
@@ -18,7 +18,7 @@ export enum SundaysCycle {
 /**
  * Weekdays cycle that can be used as metadata for liturgical days.
  */
-export enum WeekdaysCycle {
+export enum WeekdaysCycles {
   YEAR_1 = 'YEAR_1',
   YEAR_2 = 'YEAR_2',
 }
@@ -26,9 +26,42 @@ export enum WeekdaysCycle {
 /**
  * Psalter weeks that can be used as metadata for liturgical days.
  */
-export enum PsalterWeeksCycle {
+export enum PsalterWeeksCycles {
   WEEK_1 = 'WEEK_1',
   WEEK_2 = 'WEEK_2',
   WEEK_3 = 'WEEK_3',
   WEEK_4 = 'WEEK_4',
 }
+
+/**
+ * A dynamically generated constant consisting of all the enum keys in [[LITURGICAL_DAY_CYCLES]]
+ */
+export const LITURGICAL_DAY_CYCLE = Object.keys(LiturgicalDayCycles).filter(
+  (key) =>
+    typeof LiturgicalDayCycles[key as keyof typeof LiturgicalDayCycles] ===
+    'string',
+) as Array<keyof typeof LiturgicalDayCycles>;
+
+/**
+ * A dynamically generated constant consisting of all the enum keys in [[SUNDAY_CYCLES]]
+ */
+export const SUNDAYS_CYCLE = Object.keys(SundaysCycles).filter(
+  (key) => typeof SundaysCycles[key as keyof typeof SundaysCycles] === 'string',
+) as Array<keyof typeof SundaysCycles>;
+
+/**
+ * A dynamically generated constant consisting of all the enum keys in [[WEEKDAY_CYCLES]]
+ */
+export const WEEKDAYS_CYCLE = Object.keys(WeekdaysCycles).filter(
+  (key) =>
+    typeof WeekdaysCycles[key as keyof typeof WeekdaysCycles] === 'string',
+) as Array<keyof typeof WeekdaysCycles>;
+
+/**
+ * A dynamically generated constant consisting of all the enum keys in [[PSALTER_WEEKS]]
+ */
+export const PSALTER_WEEKS = Object.keys(PsalterWeeksCycles).filter(
+  (key) =>
+    typeof PsalterWeeksCycles[key as keyof typeof PsalterWeeksCycles] ===
+    'string',
+) as Array<keyof typeof PsalterWeeksCycles>;
