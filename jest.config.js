ba0504fa1f18a8865b540a6fec3eb0b7639af805
Etienne Magnier <etienne.magnier@gmail.com>
2021-08-10 00:09:13 +0200

    chore: specify ts path alias to jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,13 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
+  moduleNameMapper: {
+    '@romcal/(.*)': '<rootDir>/lib/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'lib/**/*.{ts,tsx,js,jsx}',
-    '!lib/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts'],
 };
