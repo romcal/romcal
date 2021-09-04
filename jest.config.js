f764b7724ad34d7b9c56743ff13d972e814eda3d
Etienne Magnier <etienne.magnier@gmail.com>
2021-09-04 23:00:11 +0200

    chore: cleanup package.json & make jest faster with esbuild

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    '^.+\\.tsx?$': 'ts-jest',
+    '^.+\\.tsx?$': 'esbuild-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts'],
-  coveragePathIgnorePatterns: ['lib/bundles', 'lib/locales', 'lib/particular-calendars'],
+  coveragePathIgnorePatterns: ['lib/locales', 'lib/particular-calendars'],
 };
