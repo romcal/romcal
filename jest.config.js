7bc83ebeac7682e7f67baa385703ed28900e5259
Etienne Magnier <etienne.magnier@gmail.com>
2021-09-18 18:15:48 +0200

    chore: exclude /examples/ from main library tests

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,9 @@ module.exports = {
     '^.+\\.tsx?$': 'esbuild-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['examples'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts'],
-  coveragePathIgnorePatterns: ['lib/locales', 'lib/particular-calendars'],
+  coveragePathIgnorePatterns: ['lib/locales', 'lib/particular-calendars', 'examples'],
 };
