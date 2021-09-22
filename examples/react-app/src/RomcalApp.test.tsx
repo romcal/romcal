88978a4aff44d906fd2a1ac3795c3aa89b45b07b
Etienne Magnier <etienne.magnier@gmail.com>
2021-09-22 12:05:29 +0200

    test: update test setup

--- examples/react-app/src/RomcalApp.test.tsx
+++ examples/react-app/src/RomcalApp.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
+import React from 'react';
 import RomcalApp from './RomcalApp';
 
-test('renders learn react link', () => {
+test('renders romcal headline', () => {
   render(<RomcalApp />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/A JavaScript library/i);
+  expect(textElement).toBeInTheDocument();
 });
