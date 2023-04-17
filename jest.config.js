12cfeb4bd7b0a19a8db23cf4db159f402b9d1099
Etienne Magnier <etienne.magnier@gmail.com>
2023-04-17 22:46:05 +0200

    chore: create jest-config package

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'esbuild-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  modulePathIgnorePatterns: ['examples'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts'],
-  coveragePathIgnorePatterns: ['lib/locales', 'lib/particular-calendars', 'examples'],
-  setupFilesAfterEnv: ['jest-extended/all'],
+  ...require('@romcal/jest-config'),
 };
---
+++ packages/jest-config/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'esbuild-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['examples'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts'],
+  coveragePathIgnorePatterns: ['lib/locales', 'lib/particular-calendars', 'examples'],
+  setupFilesAfterEnv: ['jest-extended/all'],
+};
