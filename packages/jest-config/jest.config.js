639a812bf177f386ac669c0c6f6efe1ef05590b4
Etienne Magnier <etienne.magnier@gmail.com>
2023-05-01 23:32:43 +0200

    chore: jest setup fine-tuning

--- packages/jest-config/jest.config.js
+++ packages/jest-config/jest.config.js
@@ -3,11 +3,18 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': ['esbuild-jest', { sourcemap: true }],
   },
+  moduleNameMapper: {
+    '@romcal/([^/]*)(.*)': '<rootDir>../$1/src$2',
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['examples'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['lib/**/*.{ts,tsx,js,jsx}', '!lib/**/*.d.ts'],
-  coveragePathIgnorePatterns: ['lib/locales', 'lib/particular-calendars', 'examples'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx,js,jsx}',
+    'build/**/*.{ts,tsx,js,jsx}',
+    '!**/types/**/*.ts',
+    '!**/index.ts',
+  ],
   setupFilesAfterEnv: ['jest-extended/all'],
 };
