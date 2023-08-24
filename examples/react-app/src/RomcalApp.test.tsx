a9f6fe6dd86aa9566b02c4ceb03e06dcb26a6085
Etienne Magnier <etienne.magnier@gmail.com>
2023-08-24 19:55:38 +0200

    chore: move examples to a dedicated github repository (#448)

--- examples/react-app/src/RomcalApp.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import React from 'react';
-import RomcalApp from './RomcalApp';
-
-test('renders romcal headline', () => {
-  render(<RomcalApp />);
-  const textElement = screen.getByText(/A JavaScript library/i);
-  expect(textElement).toBeInTheDocument();
-});
