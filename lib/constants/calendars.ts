e13fd4f131159a7efb4888946345920f128f66ef
Matt <7128721+TobiTenno@users.noreply.github.com>
2024-03-21 22:20:39 -0500

    ci: pre-commit lint, prettier, commitlint (#495)

--- lib/constants/calendars.ts
+++ lib/constants/calendars.ts
@@ -5,3 +5,5 @@ import { GENERAL_ROMAN_NAME } from './general-calendar-names';
 
 export const CALENDAR_VAR_NAMES = Object.keys(particularCalendars).concat([GENERAL_ROMAN_NAME]).sort();
 export const CALENDAR_PKG_NAMES = CALENDAR_VAR_NAMES.map((name) => toPackageName(name, true));
+
+export const CALENDAR_IDS = CALENDAR_VAR_NAMES.map((name) => toPackageName(name));
