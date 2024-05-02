42d9af0374caa06dc6091a8c421d527546f4af05
Tukusej’s Sirs <tukusejssirs@protonmail.com>
2024-05-02 04:35:15 +0200

    fix(util): fix `toScreamingSnakeCase()` function (#438)

--- lib/utils/string.spec.ts
+++ lib/utils/string.spec.ts
@@ -1,6 +1,36 @@
-import { sanitizeLocaleId } from './string';
+import { sanitizeLocaleId, toScreamingSnakeCase } from './string';
 
 describe('string.ts', () => {
+  describe('toScreamingSnakeCase', () => {
+    it('should handle strings with uppercase letters correctly', () => {
+      expect(toScreamingSnakeCase('Some RandOm strinG')).toBe('SOME_RAND_OM_STRIN_G');
+    });
+
+    it('should handle strings with no uppercase letters correctly', () => {
+      expect(toScreamingSnakeCase('some random string')).toBe('SOME_RANDOM_STRING');
+    });
+
+    it('should handle strings with no spaces', () => {
+      expect(toScreamingSnakeCase('someRandomString')).toBe('SOME_RANDOM_STRING');
+    });
+
+    it('should handle strings with multiple consequent whitespaces', () => {
+      expect(toScreamingSnakeCase('some\t\t\nrandom   string')).toBe('SOME_RANDOM_STRING');
+    });
+
+    it('should handle strings with leading and trailing whitespaces', () => {
+      expect(toScreamingSnakeCase(' \t \n some random string   \n \t')).toBe('SOME_RANDOM_STRING');
+    });
+
+    it('should handle strings with leading and trailing underscores', () => {
+      expect(toScreamingSnakeCase('__some random string__')).toBe('SOME_RANDOM_STRING');
+    });
+
+    it('should handle empty strings', () => {
+      expect(toScreamingSnakeCase('')).toBe('');
+    });
+  });
+
   describe('sanitizeLocaleId', () => {
     it('returns the locale code ID correctly formatted', () => {
       expect(sanitizeLocaleId('')).toBe('');
--- lib/utils/string.ts
+++ lib/utils/string.ts
@@ -4,9 +4,9 @@
  */
 export const toScreamingSnakeCase = (string: string): string =>
   string
-    .replace(/(.)([A-Z][a-z]+)/, '$1_$2')
-    .replace(/([A-Z][a-z]+)([0-9])/, '$1_$2')
-    .replace(/([a-z0-9])([A-Z])/, '$1_$2')
+    .replace(/[A-Z]/g, (l) => `_${l.toLowerCase()}`)
+    .replace(/[ \t\n]+_*/g, '_')
+    .replace(/(^_*|_*$)/g, '')
     .toUpperCase();
 
 /**
