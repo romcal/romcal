03c357cda2f9ff44a3f4d83db916898d7454742a
Matej Voboril <tobiah@pm.me>
2024-05-05 18:18:50 -0500

    fix(costa-rica): remove tangential addition

--- rites/roman1969/src/catalog/martyrology.ts
+++ rites/roman1969/src/catalog/martyrology.ts
@@ -3234,9 +3234,6 @@ export const Martyrology: { catalog: MartyrologyCatalog } = {
     our_lady_mother_of_divine_providence: {
       name: 'Our Lady, Mother of Divine Providence',
     },
-    our_lady_of_angels: {
-      name: 'Our Lady of Angels',
-    },
     our_lady_of_aparecida: {
       name: 'Our Lady of Aparecida',
     },
