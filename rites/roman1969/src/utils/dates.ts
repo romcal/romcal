9ec72bc92828fb47fea720ddf7f309a1b43af8f9
Matej Voboril <tobiah@pm.me>
2026-01-02 18:16:31 -0600

    style(eslint): prefer-optional-chain

--- rites/roman1969/src/utils/dates.ts
+++ rites/roman1969/src/utils/dates.ts
@@ -25,9 +25,7 @@ export const subtractsDays = (date: Date, days: number): Date => {
 
 export const isSameDate = (date1: Date, date2: Date): boolean => {
   return (
-    date1 &&
-    date2 &&
-    date1.getUTCFullYear() === date2.getUTCFullYear() &&
+    date1?.getUTCFullYear() === date2?.getUTCFullYear() &&
     date1.getUTCMonth() === date2.getUTCMonth() &&
     date1.getUTCDate() === date2.getUTCDate()
   );
