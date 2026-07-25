c7bc00f8ef424bef13e76e8be44f2c8c67f8cde1
Matej Voboril <tobiah@pm.me>
2026-07-25 15:07:42 -0500

    fix(util): use [] for types that forbid scopes
    commitlint 21 turns null into undefined and breaks selective-scope optional [null].

--- .commitlintrc.ts
+++ .commitlintrc.ts
@@ -3,7 +3,7 @@ import { UserConfig, RuleConfigSeverity } from '@commitlint/types';
 import { CALENDAR_IDS } from './rites/roman1969/src/constants/calendars';
 import { LOCALE_IDS } from './rites/roman1969/src/constants/locales';
 
-const empty = [null];
+const empty = [];
 const scopes = [...CALENDAR_IDS, ...LOCALE_IDS, 'calendar', 'l10n', 'util', 'package'];
 const styleScopes = ['commitlint', 'editorconfig', 'eslint', 'prettier'];
 
